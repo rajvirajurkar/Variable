b4d5b43c47fe0ab97ed8e543af8eaaca21e331ed
rajvi rajurkar <rajvi.vm@gmail.com>
2022-05-31 12:01:16 -0700

    Varaiable test

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName;     //Declaration
+            dogName = "Chinu";  //Initialization
+
+            int dogAge = 3;      // Declaration & Initializing
+
+            char firstInitalOfMyDog = 'C';
+
+            bool isHungry = true;
+
+            double myDogWeight = 33.3;   //decimals
+
+            decimal dogHeight = 10.5m;  //ends with m
+
+
+            Console.WriteLine($"My Dog's name is {dogName}. He is {dogAge} years old.");
+            Console.WriteLine($"He's first Inital is {firstInitalOfMyDog}. ");
+            Console.WriteLine($"He is hungry {isHungry}.");
+            Console.WriteLine($"My dog weight is {myDogWeight} lbs," +
+                $"his height is {dogHeight}");
+
+        }
+     }
+}
+
+
+
